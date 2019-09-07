61d9a08243573c60f6b87bfc4470dc284e3dfa41
Thomas Peiselt <pi@kulturguerilla.org>
2019-09-07 19:29:31 +0200

    remove additional obsoleted stuff.

--- src/ISL/Model.hs
+++ src/ISL/Model.hs
@@ -12,10 +12,6 @@ import           ML.DataSet (DataSet'(..), Feature(..), Column(..)
                             , FeatureSpace(..)
                             , FeatureSpec(..)
                             )
-import           Control.Monad.ST (runST, ST)
-import           Data.Vector (Vector, (!))
-import qualified Data.Vector as V
-import qualified Data.Vector.Mutable as VM
 import           Data.Text (Text)
 
 -- initial stub type for prediction result: the column vector of response
@@ -45,39 +41,4 @@ buildModelSpec FeatureSpace {..} responseName featureNames = do
 
 extractResponseVector :: DataSet' -> ModelSpec -> Column Double
 extractResponseVector ds ms = RU.head $ DS.featureVectors' ds (response ms)
--- split the training input so that we can use one piece as training, the
--- other part for validation.
-
-splitColumn :: [Bool] -> Column a -> (Column a, Column a)
-splitColumn idxs Column { .. } =
-    let (leftV, rightV) = splitVector idxs colData
-    in (Column colName leftV, Column colName rightV)
-
--- this actually prompted for a call for help on r/haskell:
--- https://www.reddit.com/r/haskell/comments/ckba3b/monthly_hask_anything_august_2019/eybwyig/
--- the problem is that runST introduces an s (for ST s), but the step function
--- introduces another, incompatible one. So the VM.write call wouldn't compile.
--- Solution is to introduce ScopedTypeVariables, have an explicit type
--- signature for the runST (go), bind some s using forall, and then re-using
--- that s in the type signature of the step function.  after all these changes,
--- it became possible drop the type signature for step:
-splitVector :: forall a . [Bool] -> Vector a -> (Vector a, Vector a)
-splitVector idxs v =
-    runST go
-      where
-        n         = V.length v
-        leftSize  = length (filter identity $ take n idxs)
-        rightSize = n - leftSize
-        go :: forall s . ST s (Vector a, Vector a)
-        go = do
-            lefts'  <- VM.new leftSize
-            rights' <- VM.new rightSize
-            let --  step :: Int -> Int -> Int -> ST s () -- no longer necessary, uhm.
-                step _ _ []     = return ()
-                step l r ((p,i):ps) =
-                    if p
-                       then VM.write lefts'  l (v ! i) >> step (l+1) r ps
-                       else VM.write rights' r (v ! i) >> step l (r+1) ps
-            step 0 0 $ zip idxs [0 .. n-1]
-            (,) <$> V.freeze lefts' <*> V.freeze rights'
 
--- tasty/ISL/DataSetSuite.hs
+++ tasty/ISL/DataSetSuite.hs
@@ -4,7 +4,6 @@
 module ISL.DataSetSuite where
 
 import           ML.DataSet
-import           ISL.Model
 import           Test.Tasty.Hspec (Spec)
 import           Test.Hspec
 import qualified Data.Vector as V
@@ -14,22 +13,4 @@ spec_replaceNAs =
     it "N/A values should be replaced by the variable mean" $ do
         let vs = V.fromList [-1, sqrt $ -1, 3]
         replaceNAs vs `shouldBe` V.fromList [-1, 1, 3 :: Double]
-
-spec_splitVector :: Spec
-spec_splitVector =
-    describe "splitting a vector into two" $ do
-        it "should handle empty vectors" $ do
-            let (left, right) = splitVector (repeat False) (V.empty :: V.Vector Bool)
-            left `shouldBe` V.empty
-            right `shouldBe` V.empty
-        it "should handle empty row selector" $ do
-            let inp           = V.fromList [3, 4, 5, 6 :: Int]
-                (left, right) = splitVector (repeat False) inp
-            left `shouldBe` V.empty
-            right `shouldBe` inp
-        it "should handle full row selector" $ do
-            let inp           = V.fromList [3 .. 7 :: Int]
-                (left, right) = splitVector (repeat True) inp
-            left `shouldBe` inp
-            right `shouldBe` V.empty
 
