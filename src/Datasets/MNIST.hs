9c42a56e34809a2d07d2673789b84dc5655939cc
Thomas Peiselt <pi@kulturguerilla.org>
2020-02-18 10:16:45 +0100

    read mnist data.

---
+++ src/Datasets/MNIST.hs
@@ -0,0 +1,30 @@
+module Datasets.MNIST where
+
+import qualified Data.Binary.Get      as B
+import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Vector.Storable as VS
+
+readImages :: FilePath -> IO [VS.Vector Word8]
+readImages f = do
+    bytes <- BSL.readFile f
+    return $ VS.fromList . BS.unpack <$> B.runGet parseImageFile bytes
+
+readLabels :: FilePath -> IO [Word8]
+readLabels f = do
+    bytes <- BSL.readFile f
+    return $ B.runGet parseLabelFile bytes
+
+parseImageFile :: B.Get [BS.ByteString]
+parseImageFile = do
+    _ <- B.skip 4
+    n <- fromIntegral <$> B.getWord32be
+    r <- fromIntegral <$> B.getWord32be
+    c <- fromIntegral <$> B.getWord32be
+    replicateM n $ B.getByteString (r * c)
+
+parseLabelFile :: B.Get [Word8]
+parseLabelFile = do
+    _ <- B.skip 4
+    n <- fromIntegral <$> B.getWord32be
+    replicateM n B.getWord8
